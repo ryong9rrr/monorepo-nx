73d957becfb34622a9b1e0a9bc8a0431389436a7
yong <tkddbs1644@gmail.com>
2023-12-31 20:22:27 +0900

    feat(@package/react-components-layout): Divider

---
+++ services/storybook/stories/ReactComponents/Layout/Divider.stories.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import "@package/react-components-layout/style.css";
+import { Divider as _Divider, Box } from "@package/react-components-layout";
+import { vars } from "@package/themes";
+
+const meta: Meta<typeof _Divider> = {
+  title: "React Components/Layout/Divider",
+  component: _Divider,
+  decorators: [
+    Divider => (
+      <Box padding={3} style={{ width: "300px", height: "300px" }}>
+        <Divider />
+      </Box>
+    ),
+  ],
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    orientation: {
+      options: ["horizontal", "vertical"],
+      control: "select",
+    },
+    variant: {
+      options: ["solid", "dashed"],
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof _Divider>;
+
+export const DividerStory: Story = {
+  args: {
+    color: "gray",
+    size: 1,
+    variant: "solid",
+    orientation: "horizontal",
+  },
+};
